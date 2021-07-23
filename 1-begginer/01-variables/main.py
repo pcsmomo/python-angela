ccbffb916caa91e14567ec99d6572a24300b53c5
pcsmomo <dongwoo_333@hanmail.net>
2021-07-23 17:50:38 +1000

    8. [Interactive Coding Exercise] Debugging Practice

--- 1-begginer/01-variables/main.py
+++ 1-begginer/01-variables/main.py
@@ -9,7 +9,8 @@ print("Hello Noah \nHello Noah \nHello Noah \n")
 print("String" + " " + "Concatenation")
 
 #Finally click "Run" to execute the tests
-print(Day 1 - String Manipulation")
-print("String Concatenation is done with the "+" sign.")
-  print('e.g. print("Hello " + "world")')
-print(("New lines can be created with a backslash and n.")
+print("Day 1 - String Manipulation")
+print("String Concatenation is done with the \"+\" sign.")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
