420e2c746cb09c4e9e0b1eb46edab67fecaf6c4b
pcsmomo <dongwoo_333@hanmail.net>
2021-07-23 18:04:28 +1000

    10. [Interactive Coding Exercise] Input Function

--- 1-begginer/01-variables/main.py
+++ 1-begginer/01-variables/main.py
@@ -17,3 +17,6 @@ print("New lines can be created with a backslash and n.")
 
 # input() will get user input in console
 print("Hello " + input("What is your name?\n"))
+
+# len()
+print(len(input("What is your name? ")));
