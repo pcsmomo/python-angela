a1eb4e9a6a0c045d0ccf7a796561f548d6a8a610
pcsmomo <dongwoo_333@hanmail.net>
2021-07-24 23:24:25 +1000

    41. Understanding the Offset and Appending Items to Lists

---
+++ 1-begginer/04-randomisation/main-list.py
@@ -0,0 +1,11 @@
+states_of_america = ["Dalaware", "Pennsylvania", "New Jersey"]
+
+print(states_of_america[0])
+print(states_of_america[-1])
+print(states_of_america[-2])
+
+states_of_america[1] = "Penn"
+states_of_america.append("Virginia")
+states_of_america.extend(["New York", "North Carolina"])
+
+print(states_of_america)
