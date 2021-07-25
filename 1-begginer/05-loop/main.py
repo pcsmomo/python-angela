71a53c754a57b286a669c08195dec365792d74cd
pcsmomo <dongwoo_333@hanmail.net>
2021-07-25 20:15:26 +1000

    51. for loops and the range() function

--- 1-begginer/05-loop/main.py
+++ 1-begginer/05-loop/main.py
@@ -1,7 +1,16 @@
+#For Loop
 fruits = ["Apple", "Peach", "Pear"]
-
 for fruit in fruits:
   print(fruit)
   print(f"{fruit} Pie")
 print("Outside Looop")
 # Indentation is very important
+
+#For Loop with Range(start, end:exclude, step)
+for number in range(1, 11, 3):
+  print(number)
+
+total = 0
+for number in range(1, 101):
+  total += number
+print(total)
