da09410f26f7040cc1d44e27648492b04717e412
pcsmomo <dongwoo_333@hanmail.net>
2021-07-26 21:33:23 +1000

    60. While Loops

--- 1-begginer/06-function/x-hurdle.py
+++ 1-begginer/06-function/x-hurdle.py
@@ -15,5 +15,10 @@ def jump():
   turn_right()
   move()
   turn_left()
-for step in range(6):
+# for step in range(6):
+#   jump()
+number_of_hurdles = 6
+while number_of_hurdles > 0:
   jump()
+  number_of_hurdles -= 1
+  print(number_of_hurdles)
