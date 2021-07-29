858a9437f0061a9793f6601bfdc2aca6fbf73627
pcsmomo <dongwoo_333@hanmail.net>
2021-07-29 18:39:12 +1000

    80. Positional vs. Keyword Arguments

--- 1-begginer/08-parameters/main.py
+++ 1-begginer/08-parameters/main.py
@@ -1,15 +1,28 @@
-# Review: 
-# Create a function called greet(). 
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
-
+#Simple Function
 def greet():
-    print('Hello')
-    print('How do you do')
-    print("Isn't the weather nice today??")
+  print("Hello Angela")
+  print("How do you do Jack Bauer?")
+  print("Isn't the weather nice today?")
 greet()
 
+#Function that allows for input
+#'name' is the parameter.
+#'Jack Bauer' is the argument.
 def greet_with_name(name):
-    print(f"Hello {name}")
-    print(f"How do you do {name}?")
-greet_with_name("Noah")
+  print(f"Hello {name}")
+  print(f"How do you do {name}?")
+greet_with_name("Jack Bauer")
+
+#Functions with more than 1 input
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"What is it like in {location}?")
+
+#Calling greet_with() with Positional Arguments
+greet_with("Jack Bauer", "Nowhere")
+#vs.
+greet_with("Nowhere", "Jack Bauer")
+
+
+#Calling greet_with() with Keyword Arguments
+greet_with(location="London", name="Angela")
