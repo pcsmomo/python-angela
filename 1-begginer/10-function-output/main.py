9fb4b07921b572c96992495028afad512d4ddb75
pcsmomo <dongwoo_333@hanmail.net>
2021-08-02 08:27:29 +1000

    101. Calculator Part 1: Combining Dictionaries and Functions

--- 1-begginer/10-function-output/main.py
+++ 1-begginer/10-function-output/main.py
@@ -17,6 +17,7 @@ length = len(formatted_name)
 
 #Return as an early exit
 def format_name(f_name, l_name):
+  # Docstrings
   """Take a first and last name and format it 
   to return the title case version of the name."""
   if f_name == "" or l_name == "":
