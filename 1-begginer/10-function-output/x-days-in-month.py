8644627676eb6ae92dec1bc033113641f9b8b256
pcsmomo <dongwoo_333@hanmail.net>
2021-08-02 08:19:49 +1000

    99. [Interactive Coding Exercise] Days in Month

--- 1-begginer/10-function-output/x-days-in-month.py
+++ 1-begginer/10-function-output/x-days-in-month.py
@@ -2,19 +2,24 @@ def is_leap(year):
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
-        print("Leap year.")
+        return True
       else:
-        print("Not leap year.")
+        return False
     else:
-      print("Leap year.")
+        return True
   else:
-    print("Not leap year.")
+    return False
+    
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if month > 12 or month < 1:
+    return "Invalid month entered."
+  if month == 2 and is_leap(year):
+    return 29
+  return month_days[month - 1]
 
-def days_in_month():
-  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
-  
-  
-#🚨 Do NOT change any of the code below 
+
+#Do NOT change any of the code below 👇
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
