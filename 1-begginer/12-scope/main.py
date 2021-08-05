743eb1dca69a4d45ed8c2a076800d53161940b16
pcsmomo <dongwoo_333@hanmail.net>
2021-08-06 06:56:25 +1000

    116. How to Modify a Global Variable

--- 1-begginer/12-scope/main.py
+++ 1-begginer/12-scope/main.py
@@ -7,7 +7,7 @@ def increase_enemies():
   enemies = 2
   print(f"enemies inside function: {enemies}")
   if game_level < 5:
-      new_enemy = enemies[0]
+      new_enemy = 'new enemy'
   print(new_enemy)
 
 increase_enemies()
@@ -15,3 +15,20 @@ print(f"enemies outside function: {enemies}")
 
 #Out of scope
 # print(new_enemy)
+
+
+################### Scope ####################
+
+enemies = 1
+
+# def increase_enemies():
+#   global enemies
+#   enemies += 1
+#   print(f"enemies inside function: {enemies}")
+
+def increase_enemies():
+  print(f"enemies inside function: {enemies}")
+  return enemies + 1
+
+enemies = increase_enemies()
+print(f"enemies outside function: {enemies}")
