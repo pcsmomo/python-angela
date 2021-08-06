66e395ecc20b2f03c64c51352cb153996cd1e893
pcsmomo <dongwoo_333@hanmail.net>
2021-08-06 21:59:00 +1000

    129. [Interactive Coding Exercise] Debugging Leap Year

--- 1-begginer/13-debugging/debugging-fizzbuzz.py
+++ 1-begginer/13-debugging/debugging-fizzbuzz.py
@@ -1,6 +1,9 @@
-number = int(input("Which number do you want to check?"))
-
-if number % 2 = 0:
-  print("This is an even number.")
-else:
-  print("This is an odd number.")
+for number in range(1, 101):
+  if number % 3 == 0 or number % 5 == 0:
+    print("FizzBuzz")
+  if number % 3 == 0:
+    print("Fizz")
+  if number % 5 == 0:
+    print("Buzz")
+  else:
+    print([number])
