956cf8e8cad2c3f9d1c559b61b4e530a2c220614
pcsmomo <dongwoo_333@hanmail.net>
2021-08-10 09:04:08 +1000

    153. Working with Attributes, Class Constructors and the __init__() Function

--- 2-intermediate/17-OOP-benefits/main.py
+++ 2-intermediate/17-OOP-benefits/main.py
@@ -1,4 +1,19 @@
 class User:
-    pass
+    # Constructor
+    def __init__(self, user_id, username):
+        print("new user being created...");
+        self.id = user_id
+        self.username = username
+        self.followers = 0
 
-user_1 = User()
+user_1 = User('001', 'Noah')
+# user_1.id = '001'
+# user_1.username = "Noah"
+
+print(user_1.username)
+print(user_1.followers)
+
+user_2 = User('002', 'Jack')
+# user_2.id = '002'
+# user_2.username = "jack"
+print(user_2.id)
