9b1dddfc116ed4e93ef7ea7487ef07c9f2345740
pcsmomo <dongwoo_333@hanmail.net>
2021-08-10 08:35:18 +1000

    148. Building the Coffee Machine in OOP

--- 2-intermediate/16-OOP/main.py
+++ 2-intermediate/16-OOP/main.py
@@ -1,24 +1,3 @@
-# import another_module
-# print(another_module.another_variable)
-
-# from turtle import Turtle, Screen
-# timmy = Turtle()
-# print(timmy)
-# timmy.shape("turtle")
-# timmy.color("orchid")
-# timmy.forward(100)
-
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
-
-from prettytable import PrettyTable
-table = PrettyTable()
-
-table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
-table.add_column("Type", ["Electric", "Water", "Fire"])
-
-table.align = "l"
-
-print(table.align)
-print(table)
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
