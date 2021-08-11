6feb928e9f9d449fce16f328ca00d7cfd8e6cd47
pcsmomo <dongwoo_333@hanmail.net>
2021-08-12 06:27:01 +1000

    164. Turtle Challenge 1 - Draw a Square

--- 2-intermediate/18-turtle-GUI/main.py
+++ 2-intermediate/18-turtle-GUI/main.py
@@ -1,21 +1,16 @@
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape('turtle')
-timmy_the_turtle.color('red')
-timmy_the_turtle.forward(180)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(50)
+tim = Turtle()
+# timmy_the_turtle.shape('turtle')
+# timmy_the_turtle.color('red')
+# timmy_the_turtle.forward(180)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.forward(50)
 
-
-
-
-
-
-
-
-
-
+# Drawing square
+for _ in range(4):
+    tim.forward(180)
+    tim.right(90)
 
 screen = Screen()
 screen.exitonclick()
