f89cc6abb63a918f49bc924dbd0902031a3dc328
pcsmomo <dongwoo_333@hanmail.net>
2021-08-12 06:37:47 +1000

    165. Importing Modules, Installing Packages, and Working with Aliases

---
+++ 2-intermediate/18-turtle-GUI/main2.py
@@ -0,0 +1,14 @@
+# Not recommended to import this way
+# from turtle import *
+# from random import *
+
+# print(choice([1,2,3]))
+
+# from turtle import Turtle
+
+# Using alias
+import turtle as t
+tim = t.Turtle()
+
+import heroes
+print(heroes.gen())
