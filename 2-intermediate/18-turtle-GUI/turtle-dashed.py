8e7aa843ca7f8826cf509cef416e9bf6c18bd46d
pcsmomo <dongwoo_333@hanmail.net>
2021-08-12 06:39:50 +1000

    166. Turtle Challenge 2 - Draw a Dashed Line

---
+++ 2-intermediate/18-turtle-GUI/turtle-dashed.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+tim = t.Turtle()
+
+########### Challenge 2 - Draw a Dashed Line ########
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+screen = t.Screen()
+screen.exitonclick()
