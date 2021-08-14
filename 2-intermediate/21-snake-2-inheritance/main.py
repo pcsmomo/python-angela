b74b783662218657d7319102ce383db51d02c667
pcsmomo <dongwoo_333@hanmail.net>
2021-08-15 08:01:53 +1000

    190. Create a Scoreboard and Keep Score

--- 2-intermediate/21-snake-2-inheritance/main.py
+++ 2-intermediate/21-snake-2-inheritance/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -28,5 +30,6 @@ while game_is_on:
     if snake.head.distance(food) < 15:
         print("nom nom nom")
         food.refresh()
+        scoreboard.increase_score()
 
 screen.exitonclick()
