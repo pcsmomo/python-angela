5232d7e07e41aeb8f5f0743a78295bf59b7a1631
pcsmomo <dongwoo_333@hanmail.net>
2021-08-28 19:42:18 +1000

    228. U.S. States Game Part 1: Setup

---
+++ 2-intermediate/25-pandas-csv/z_us-states-game/main.py
@@ -0,0 +1,18 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+turtle.addshape(image)
+turtle.shape(image)
+
+# Get mouse coordinate
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
+
+answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+print(answer_state)
+
+screen.exitonclick()
