84f0071bc084f3f9d0fa54455c34ab654a88e579
pcsmomo <dongwoo_333@hanmail.net>
2021-09-16 18:46:03 +1000

    268. Catching Exceptions: The try catch except finally Pattern

---
+++ 2-intermediate/30-errors-exceptions-json/main.py
@@ -0,0 +1,18 @@
+# FileNotFound
+
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    # print(a_dictionary["sdfsdf"])
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
