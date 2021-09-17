f5add7368bb7e4bc2fe1b86240b8acddd49b6695
pcsmomo <dongwoo_333@hanmail.net>
2021-09-17 18:04:53 +1000

    274. Challenge 1 - Handling Exceptions in the Password Manager

--- 2-intermediate/30-errors-exceptions-json/password-manager/main.py
+++ 2-intermediate/30-errors-exceptions-json/password-manager/main.py
@@ -46,13 +46,21 @@ def save():
         # with open("data.json", "r") as data_file:
         #     data = json.load(data_file)
         #     print(data)
-        with open("data.json", "r") as data_file:
-            # Reading old data
-            data = json.load(data_file)
+        try:
+            with open("data.json", "r") as data_file:
+                # Reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
             # Updating old data with data
             data.update(new_data)
-        with open("data.json", "w") as data_file:
-            json.dump(data, data_file, indent=4)
+
+            with open("data.json", "w") as data_file:
+                # Saving updated data
+                json.dump(data, data_file, indent=4)
+        finally:
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
