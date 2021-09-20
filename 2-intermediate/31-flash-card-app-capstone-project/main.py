e20afce62874124cea16bf8d920f52c9b58c7f5f
pcsmomo <dongwoo_333@hanmail.net>
2021-09-21 06:13:29 +1000

    284. Solution & Walkthrough for Saving Progress

--- 2-intermediate/31-flash-card-app-capstone-project/main.py
+++ 2-intermediate/31-flash-card-app-capstone-project/main.py
@@ -3,10 +3,16 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/cantonese_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
+to_learn = {}
+
+try:
+    data = pandas.read_csv("data/cantonese_words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/cantonese_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def next_card():
@@ -33,6 +39,13 @@ def flip_card():
     canvas.itemconfig(card_background2, image=card_back_img)
 
 
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/cantonese_words_to_learn.csv", index=False)
+    next_card()
+
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
@@ -57,7 +70,7 @@ unknown_button = Button(image=cross_image, highlightthickness=0, command=next_ca
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
