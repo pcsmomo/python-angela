6b9727d3def50b506214d73a30e3b7efeea3416c
pcsmomo <dongwoo_333@hanmail.net>
2021-10-15 17:37:55 +1100

    320. Solution & Walkthrough for Step 3 - Send the SMS Messages

--- 3-intermediate+/36-stock-trading/main.py
+++ 3-intermediate+/36-stock-trading/main.py
@@ -1,4 +1,5 @@
 import requests
+from twilio.rest import Client
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -6,8 +7,10 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-STOCK_API_KEY = "5MO3W2KGEJEDACUV"
-NEWS_API_KEY = "8a128d77a3c44b85947dd473260acd0d"
+STOCK_API_KEY = "<STOCK_API_KEY>"
+NEWS_API_KEY = "<NEWS_API_KEY>"
+TWILIO_SID = "<TWILIO_SID>"
+TWILIO_AUTH_TOKEN = "<TWILIO_AUTH_TOKEN>"
 
 ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -31,17 +34,24 @@ day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
 # Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20. Hint: https://www.w3schools.com/python/ref_func_abs.asp
-difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+# difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
+
 print(difference)
 
 # Work out the percentage difference in price between closing price yesterday and closing price the day before yesterday.
-diff_percent = (difference / float(yesterday_closing_price)) * 100
+diff_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(diff_percent)
 
 ## STEP 2: https://newsapi.org/
 # Instead of printing ("Get News"), use the News API to get articles related to the COMPANY_NAME.
 # if diff_percent > 5:
-if diff_percent > 0.5:
+if abs(diff_percent) > 0.5:
     news_params = {
         "apiKey": NEWS_API_KEY,
         "qInTitle": COMPANY_NAME
@@ -53,21 +63,20 @@ if diff_percent > 0.5:
     three_articles = articles[:3]
     print(three_articles)
 
-## STEP 3: Use twilio.com/docs/sms/quickstart/python
-# to send a separate message with each article's title and description to your phone number.
+    ## STEP 3: Use twilio.com/docs/sms/quickstart/python
+    # to send a separate message with each article's title and description to your phone number.
 
-# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+    # Create a new list of the first 3 article's headline and description using list comprehension.
+    # Optional: Format the message like this:
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\n"
+                          f"Headline: {article['title']}. \n"
+                          f"Brief: {article['description']}" for article in three_articles]
 
-# TODO 9. - Send each article as a separate message via Twilio.
-
-
-# Optional TODO: Format the message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+    # Send each article as a separate message via Twilio.
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_="+18783484107",
+            to="+61"
+        )
