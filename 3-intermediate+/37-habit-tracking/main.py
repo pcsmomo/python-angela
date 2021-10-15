5caa0c4548b5f06e18b55779c4fe14a36e59e6a0
pcsmomo <dongwoo_333@hanmail.net>
2021-10-15 18:34:39 +1100

    323. Advanced Authentication using an HTTP Header

--- 3-intermediate+/37-habit-tracking/main.py
+++ 3-intermediate+/37-habit-tracking/main.py
@@ -1,14 +1,39 @@
 import requests
 
+USERNAME = "noahkim"
+TOKEN = "dkowiclasqjciasdfioeiraslkj"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "dkowiclasqjciasdfioeiraslkj",
-    "username": "noahkim",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes"
 }
 
-# Create my account
+# Step 1. Create my account
 # response = requests.post(url=pixela_endpoint, json=user_params)
 # print(response.text)
+# # {"message":"Success.","isSuccess":true}
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Reading Graph",
+    "unit": "page",
+    "type": "int",
+    "color": "momiji"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# Step 2. Create my graph
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+# # {"message":"Success.","isSuccess":true}
+
+# Step 3. https://pixe.la/v1/users/noahkim/graphs/graph1.html
