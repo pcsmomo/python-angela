fe5677a01ef14d9b8181a855706325e665b5f925
pcsmomo <dongwoo_333@hanmail.net>
2021-11-25 17:39:41 +1100

    407. Step 4 - Creating and Adding to Spotify Playlist

--- 5-intermediate++/46-spotify-playlist/main.py
+++ 5-intermediate++/46-spotify-playlist/main.py
@@ -6,12 +6,12 @@ from spotipy.oauth2 import SpotifyOAuth
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
 # 2013-12-31
 
-response = requests.get('https://www.billboard.com/charts/hot-100/' + date)
+response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 song_names_h3s = soup.find_all("h3", class_="c-title")
 song_names = [song.getText().strip() for song in song_names_h3s]
-# print(song_names)
+print(song_names)
 
 # Spotify Authentication
 sp = spotipy.Spotify(
@@ -25,7 +25,7 @@ sp = spotipy.Spotify(
     )
 )
 user_id = sp.current_user()["id"]
-# print(user_id)
+print(user_id)
 
 # Searching Spotify for songs by title
 song_uris = []
@@ -38,3 +38,10 @@ for song in song_names:
         song_uris.append(uri)
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
+
+# Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+print(playlist)
+
+# Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
