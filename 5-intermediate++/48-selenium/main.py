f7abe1a18582c67862c08ee5c7e2b1e620d24da9
pcsmomo <dongwoo_333@hanmail.net>
2021-11-29 18:14:14 +1100

    replace the deprecated option

--- 5-intermediate++/48-selenium/main.py
+++ 5-intermediate++/48-selenium/main.py
@@ -1,9 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
-chrome_driver_path = '/Users/noah/Documents/Study/Study_codes/udemy/python-angela/resources/5-intermediate++/48-selenium-webdriver/chromedriver'
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+chrome_driver_path = '/Users/noah/Documents/Study/Study_codes/udemy/python-angela/resources/5-intermediate++/' \
+                     '48-selenium-webdriver/chromedriver'
+service = Service(chrome_driver_path)
+# driver = webdriver.Chrome(executable_path=chrome_driver_path)  # deprecated
+driver = webdriver.Chrome(service=service)
 
 driver.get("https://www.amazon.com")
 
 # driver.close()  # close one tab
-driver.quit()   # close whole window
+driver.quit()  # close whole window
