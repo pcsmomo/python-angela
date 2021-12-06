be77120f59f1071b496753138092ce99601034f4
pcsmomo <dongwoo_333@hanmail.net>
2021-12-06 17:58:48 +1100

    422. Step 3 - Apply for a Job

--- 5-intermediate++/49-job-linkedin/main.py
+++ 5-intermediate++/49-job-linkedin/main.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 ACCOUNT_EMAIL = YOUR_LOGIN_EMAIL
 ACCOUNT_PASSWORD = YOUR_LOGIN_PASSWORD
+PHONE = YOUR PHONE NUMBER
 
 chrome_driver_path = '/Users/noah/Documents/Study/Study_codes/udemy/python-angela/resources/chromedriver'
 service = Service(chrome_driver_path)
@@ -23,3 +25,18 @@ email_field.send_keys(ACCOUNT_EMAIL)
 password_field = driver.find_element_by_id("password")
 password_field.send_keys(ACCOUNT_PASSWORD)
 password_field.send_keys(Keys.ENTER)
+
+#Locate the apply button
+time.sleep(5)
+apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
+apply_button.click()
+
+#If application requires phone number and the field is empty, then fill in the number.
+time.sleep(5)
+phone = driver.find_element_by_class_name("fb-single-line-text__input")
+if phone.text == "":
+    phone.send_keys(PHONE)
+
+#Submit the application
+submit_button = driver.find_element_by_css_selector("footer button")
+submit_button.click()
